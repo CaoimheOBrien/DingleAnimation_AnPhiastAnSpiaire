c37f0e1d50c415d1f21b1472198caa84ddeddb0e
Caoimhe <c00297178@setu.ie>
2025-10-08 09:41:58 +0100

    Added Service Worker

---
+++ sw.js
@@ -0,0 +1,61 @@
+//Service Worker 
+const CACHE_NAME = "AnPhiastAnSpiaire";
+
+const FILES_TO_CACHE =[
+    "/DingleAnimation_AnPhiastAnSpiaire/",
+    "/DingleAnimation_AnPhiastAnSpiaire/manifest.json",
+
+    //Pages 
+    "/DingleAnimation_AnPhiastAnSpiaire/index.html",
+
+    //Scripts
+    "/DingleAnimation_AnPhiastAnSpiaire/main.js",
+
+    //StyleSheets
+    "/DingleAnimation_AnPhiastAnSpiaire/assets/style/stylesheet.css",
+
+    //Icons 
+
+    //Assets 
+
+    //Images 
+
+
+    //Audio 
+
+];
+
+// Install event: cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: serve cached files when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
