acd981aca2259140785d17bd3aa04ca6458af29e
Natalia Ryl <c00294480@setu.ie>
2025-10-21 22:18:18 +0100

    set up scene 3

---
+++ assets/scripts/cutscene3.js
@@ -0,0 +1,10 @@
+//  Dingle Animation Game Prototype: An Phiast An Spiaire
+
+// Author: Caoimhe O'Brien, Aoife Leahy and Natalia Ryl 
+// Primary Programmer: Natalia Ryl
+// Date: 21/10/2025
+
+const canvas = document.getElementById("the_canvas")
+const context = canvas.getContext("2d");
+
+//--------------------------------------------------------------------------------------------------------------------
