bb241404a5c8298ff2d60e89a5d23b8f93811f49
Caoimhe <c00297178@setu.ie>
2025-10-24 17:47:28 +0100

    Fixed live site issue with button

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = () => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./sw.js');
     }
-  }
+  
 
 document.getElementById('startBtn').addEventListener('click', function(){
   window.location.href='storyInfo.html';
@@ -14,12 +14,4 @@ document.getElementById('menuBtn').addEventListener('click', function(){
   window.location.href='menu.html';
 })
 
-document.getElementById('3').addEventListener('click', function(){
-  window.location.href='puzzlePage3.html';
-})
-
-
-// Check what level it is.
-import { GameState } from "/assets/scripts/level.js";
-
-console.log("Current level is", GameState.level);
+}
